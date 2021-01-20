f2de5a935b7a8d63a9c41b7e058de6552c4aa1b3
Jorge Menéndez Sánchez <jorge@MacBook-Pro-de-Usuario.local>
2021-01-20 22:34:06 +0100

    scripts

---
+++ actualizaoca.sh
@@ -0,0 +1,459 @@
+#script para actualizar los repositorios de OCA 
+
+cd /opt/odoo
+git fetch origin 13.0
+git pull origin 13.0
+
+cd /opt/oca/account-analytic
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/account-budgeting
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/account-closing
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/account-consolidation
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/account-financial-reporting
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/account-financial-tools
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/account-fiscal-rule
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/account-invoice-reporting
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/account-invoicing
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/account-payment
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/account-reconcile
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/apps-store
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+
+cd /opt/oca/bank-payment
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/bank-statement-import
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/brand
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/business-requirement
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+
+cd /opt/oca/calendar
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/commission
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/community-data-files
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/contract
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/credit-control
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/crm
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/currency
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+
+cd /opt/oca/data-protection
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/ddmrp
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/delivery-carrier
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/dms
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/donation
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+
+cd /opt/oca/e-commerce
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/edi
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/event
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+
+cd /opt/oca/field-service
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+# cd /opt/oca/fleet
+# git fetch origin 13.0
+git pull origin 13.0
+
+cd /opt/oca/geospatial
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+
+cd /opt/oca/helpdesk
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/hr
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/hr-attendance
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/hr-expense
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/hr-holidays
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+
+cd /opt/oca/interface-github
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/intrastat-extrastat
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/iot
+git fetch origin 13.0
+git pull origin 13.0
+
+cd /opt/oca/knowledge
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+
+cd /opt/oca/l10n-spain
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+
+cd /opt/oca/maintenance
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/management-system
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/manufacture
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/manufacture-reporting
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/margin-analysis
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/mis-builder	
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/mis-builder-contrib
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/multi-company
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+
+cd /opt/oca/operating-unit
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+
+cd /opt/oca/partner-contact
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/payroll
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/pos
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/product-attribute
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/product-kitting
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/product-pack
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/product-variant
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/project	
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/project-agile
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/project-reporting
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/purchase-reporting
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/purchase-workflow
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+
+cd /opt/oca/queue
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+
+cd /opt/oca/report-print-send
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/reporting-engine
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/rest-framework
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/rma
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+
+cd /opt/oca/sale-financial
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/sale-reporting
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/sale-workflow
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/search-engine
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/server-auth	
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/server-backend
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/server-brand
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/server-env
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/server-tools
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/server-ux
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/social
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/stock-logistics-barcode
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/stock-logistics-reporting
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/stock-logistics-tracking
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/stock-logistics-transport
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/stock-logistics-warehouse
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/stock-logistics-workflow
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/survey
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+
+cd /opt/oca/timesheet
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+
+cd /opt/oca/vertical-abbey
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/vertical-agriculture
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/vertical-association
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/vertical-community
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/vertical-construction
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/vertical-edition
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/vertical-education
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/vertical-hotel
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/vertical-isp
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/vertical-medical
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/vertical-ngo
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/vertical-realestate
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/vertical-travel
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+
+cd /opt/oca/web
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/webhook
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/webkit-tools
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/website
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/website-cms
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/website-themes
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
+cd /opt/oca/wms
+git checkout .
+git fetch origin 13.0
+git pull origin 13.0
---
+++ odoo-oca-http.sh
@@ -0,0 +1,146 @@
+#script para clonar el core y todos los repositorios OCA versión 13
+
+git clone -b 13.0 https://github.com/OCA/OCB   							    	./odoo
+
+git clone -b 13.0 https://github.com/OCA/account-analytic						./oca/account-analytic
+git clone -b 13.0 https://github.com/OCA/account-budgeting 						./oca/account-budgeting
+git clone -b 13.0 https://github.com/OCA/account-consolidation 					./oca/account-consolidation
+git clone -b 13.0 https://github.com/OCA/account-financial-reporting 			./oca/account-financial-reporting
+git clone -b 13.0 https://github.com/OCA/account-financial-tools 				./oca/account-financial-tools
+git clone -b 13.0 https://github.com/OCA/account-invoice-reporting 				./oca/account-invoice-reporting
+git clone -b 13.0 https://github.com/OCA/account-invoicing 						./oca/account-invoicing
+git clone -b 13.0 https://github.com/OCA/account-payment 						./oca/account-payment
+git clone -b 13.0 https://github.com/OCA/account-reconcile 						./oca/account-reconcile
+git clone -b 13.0 https://github.com/OCA/account-closing 						./oca/account-closing
+git clone -b 13.0 https://github.com/OCA/account-fiscal-rule 					./oca/account-fiscal-rule
+git clone -b 13.0 https://github.com/OCA/apps-store 							./oca/apps-store
+
+git clone -b 13.0 https://github.com/OCA/bank-payment 							./oca/bank-payment
+git clone -b 13.0 https://github.com/OCA/bank-statement-import					./oca/bank-statement-import
+git clone -b 13.0 https://github.com/OCA/business-requirement 					./oca/business-requirement
+git clone -b 13.0 https://github.com/OCA/brand         					./oca/brand
+
+git clone -b 13.0 https://github.com/OCA/commission 							./oca/commission
+git clone -b 13.0 https://github.com/OCA/community-data-files 					./oca/community-data-files
+git clone -b 13.0 https://github.com/OCA/contract 								./oca/contract
+git clone -b 13.0 https://github.com/OCA/connector 								./oca/connector
+git clone -b 13.0 https://github.com/OCA/connector-prestashop 					./oca/connector-prestashop
+git clone -b 13.0 https://github.com/OCA/connector-woocommerce 					./oca/connector-woocommerce
+git clone -b 13.0 https://github.com/OCA/connector-odoo2odoo 					./oca/connector-odoo2odoo
+git clone -b 13.0 https://github.com/OCA/connector-lims 						./oca/connector-lims
+git clone -b 13.0 https://github.com/OCA/connector-accountedge 					./oca/connector-accountedge
+
+git clone -b 13.0 https://github.com/OCA/crm 									./oca/crm
+git clone -b 13.0 https://github.com/OCA/currency 								./oca/currency
+git clone -b 13.0 https://github.com/OCA/connector-telephony 					./oca/connector-telephony
+git clone -b 13.0 https://github.com/OCA/calendar 								./oca/calendar
+git clone -b 13.0 https://github.com/OCA/connector-ecommerce 					./oca/connector-ecommerce
+git clone -b 13.0 https://github.com/OCA/connector-magento 						./oca/connector-magento
+git clone -b 13.0 https://github.com/OCA/credit-control 						./oca/credit-control
+git clone -b 13.0 https://github.com/OCA/connector-lengow 						./oca/connector-lengow
+git clone -b 13.0 https://github.com/OCA/connector-salesforce 					./oca/connector-salesforce
+git clone -b 13.0 https://github.com/OCA/connector-sage 						./oca/connector-sage
+git clone -b 13.0 https://github.com/OCA/connector-infor 						./oca/connector-infor
+git clone -b 13.0 https://github.com/OCA/connector-redmine 						./oca/connector-redmine
+git clone -b 13.0 https://github.com/OCA/connector-interfaces 					./oca/connector-interfaces
+git clone -b 13.0 https://github.com/OCA/connector-cmis 						./oca/connector-cmis
+git clone -b 13.0 https://github.com/OCA/connector-spscommerce 					./oca/connector-spscommerce
+
+git clone -b 13.0 https://github.com/OCA/donation 								./oca/donation
+git clone -b 13.0 https://github.com/OCA/ddmrp 									./oca/ddmrp
+git clone -b 13.0 https://github.com/OCA/delivery-carrier 					./oca/delivery-carrier
+git clone -b 13.0 https://github.com/OCA/data-protection 						./oca/data-protection
+
+git clone -b 13.0 https://github.com/OCA/edi 									./oca/edi
+git clone -b 13.0 https://github.com/OCA/event 									./oca/event
+git clone -b 13.0 https://github.com/OCA/e-commerce 							./oca/e-commerce
+
+git clone -b 13.0 https://github.com/OCA/field-service 							./oca/field-service
+
+
+git clone -b 13.0 https://github.com/OCA/geospatial 							./oca/geospatial
+
+git clone -b 13.0 https://github.com/OCA/hr 									./oca/hr
+git clone -b 13.0 https://github.com/OCA/helpdesk 								./oca/helpdesk
+
+git clone -b 13.0 https://github.com/OCA/intrastat-extrastat 					./oca/intrastat-extrastat
+git clone -b 13.0 https://github.com/OCA/iot 									./oca/iot
+git clone -b 13.0 https://github.com/OCA/infrastructure-dns 					./oca/infrastructure-dns
+git clone -b 13.0 https://github.com/OCA/interface-github 						./oca/interface-github
+
+git clone -b 13.0 https://github.com/OCA/knowledge 								./oca/knowledge
+
+git clone -b 13.0 https://github.com/OCA/l10n-spain 							./oca/l10n-spain
+
+git clone -b 13.0 https://github.com/OCA/manufacture 							./oca/manufacture
+git clone -b 13.0 https://github.com/OCA/maintenance 							./oca/maintenance
+git clone -b 13.0 https://github.com/OCA/multi-company 							./oca/multi-company
+
+git clone -b 13.0 https://github.com/OCA/manufacture-reporting 					./oca/manufacture-reporting
+git clone -b 13.0 https://github.com/OCA/management-system 						./oca/management-system
+git clone -b 13.0 https://github.com/OCA/mis-builder  							./oca/mis-builder
+git clone -b 13.0 https://github.com/OCA/margin-analysis 						./oca/margin-analysis
+git clone -b 13.0 https://github.com/OCA/mis-builder-contrib 					./oca/mis-builder-contrib
+
+git clone -b 13.0 https://github.com/OCA/operating-unit 						./oca/operating-unit
+
+git clone -b 13.0 https://github.com/OCA/pos 									./oca/pos
+git clone -b 13.0 https://github.com/OCA/purchase-workflow 						./oca/purchase-workflow
+git clone -b 13.0 https://github.com/OCA/project 								./oca/project
+git clone -b 13.0 https://github.com/OCA/product-attribute 						./oca/product-attribute
+git clone -b 13.0 https://github.com/OCA/product-pack 						./oca/product-pack
+git clone -b 13.0 https://github.com/OCA/partner-contact 						./oca/partner-contact
+git clone -b 13.0 https://github.com/OCA/product-variant 						./oca/product-variant
+
+git clone -b 13.0 https://github.com/OCA/product-kitting 						./oca/product-kitting
+git clone -b 13.0 https://github.com/OCA/project-agile 							./oca/project-agile
+git clone -b 13.0 https://github.com/OCA/purchase-reporting 					./oca/purchase-reporting
+git clone -b 13.0 https://github.com/OCA/project-reporting 						./oca/project-reporting
+
+git clone -b 13.0 https://github.com/OCA/queue 									./oca/queue
+
+git clone -b 13.0 https://github.com/OCA/reporting-engine 						./oca/reporting-engine
+git clone -b 13.0 https://github.com/OCA/rest-framework 						./oca/rest-framework
+git clone -b 13.0 https://github.com/OCA/report-print-send 						./oca/report-print-send
+git clone -b 13.0 https://github.com/OCA/rma 									./oca/rma
+
+git clone -b 13.0 https://github.com/OCA/server-ux 								./oca/server-ux
+git clone -b 13.0 https://github.com/OCA/social 								./oca/social
+git clone -b 13.0 https://github.com/OCA/server-tools 							./oca/server-tools
+git clone -b 13.0 https://github.com/OCA/sale-workflow 							./oca/sale-workflow
+git clone -b 13.0 https://github.com/OCA/server-auth 							./oca/server-auth
+git clone -b 13.0 https://github.com/OCA/stock-logistics-warehouse 			./oca/stock-logistics-warehouse
+git clone -b 13.0 https://github.com/OCA/survey 								./oca/survey
+git clone -b 13.0 https://github.com/OCA/stock-logistics-workflow 			./oca/stock-logistics-workflow
+git clone -b 13.0 https://github.com/OCA/server-env 							./oca/server-env
+git clone -b 13.0 https://github.com/OCA/search-engine 						./oca/search-engine
+git clone -b 13.0 https://github.com/OCA/server-backend 						./oca/server-backend
+git clone -b 13.0 https://github.com/OCA/stock-logistics-barcode 				./oca/stock-logistics-barcode
+git clone -b 13.0 https://github.com/OCA/sale-reporting 						./oca/sale-reporting
+git clone -b 13.0 https://github.com/OCA/stock-logistics-tracking 			./oca/stock-logistics-tracking
+git clone -b 13.0 https://github.com/OCA/server-brand 						./oca/server-brand
+git clone -b 13.0 https://github.com/OCA/stock-logistics-transport 			./oca/stock-logistics-transport
+git clone -b 13.0 https://github.com/OCA/sale-financial 						./oca/sale-financial
+git clone -b 13.0 https://github.com/OCA/stock-logistics-reporting 			./oca/stock-logistics-reporting
+
+git clone -b 13.0 https://github.com/OCA/timesheet 							./oca/timesheet
+
+git clone -b 13.0 https://github.com/OCA/vertical-association 				./oca/vertical-association
+git clone -b 13.0 https://github.com/OCA/vertical-hotel 						./oca/vertical-hotel
+#git clone -b 13.0 https://github.com/OCA/vertical-medical 						./oca/vertical-medical
+git clone -b 13.0 https://github.com/OCA/vertical-abbey 						./oca/vertical-abbey
+git clone -b 13.0 https://github.com/OCA/vertical-travel 						./oca/vertical-travel
+git clone -b 13.0 https://github.com/OCA/vertical-ngo 						./oca/vertical-ngo
+git clone -b 13.0 https://github.com/OCA/vertical-isp 						./oca/vertical-isp
+git clone -b 13.0 https://github.com/OCA/vertical-construction 				./oca/vertical-construction
+git clone -b 13.0 https://github.com/OCA/vertical-education 					./oca/vertical-education
+git clone -b 13.0 https://github.com/OCA/vertical-agriculture 				./oca/vertical-agriculture
+#git clone -b 13.0 https://github.com/OCA/vertical-realestate 					./oca/vertical-realestate
+
+git clone -b 13.0 https://github.com/OCA/website 								./oca/website
+git clone -b 13.0 https://github.com/OCA/website-cms 							./oca/website-cms
+git clone -b 13.0 https://github.com/OCA/website-themes 						./oca/website-themes
+git clone -b 13.0 https://github.com/OCA/webhook 								./oca/webhook
+git clone -b 13.0 https://github.com/OCA/web 									./oca/web
+git clone -b 13.0 https://github.com/OCA/webkit-tools 						./oca/webkit-tools
+git clone -b 13.0 https://github.com/OCA/wms         					        ./oca/wms
